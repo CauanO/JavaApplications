485fa8ecdca9ac5ac7a419e60fa555d885e48cb2
Cauan Oliveira <134656319+CauanO@users.noreply.github.com>
2024-10-28 18:39:56 -0300

    Update and rename JavaAplicationQuinze.java to PesoIdeal.java

---
+++ src/PesoIdeal.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PesoIdeal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("1: Masculino | 2: Feminino");
+        int sexo = sc.nextInt();
+        System.out.println("Digite sua altura: ");
+        float altura = sc.nextFloat();
+        if (sexo == 1) {
+            double pesoIdealMasculino = (altura * 72.7) - 58;
+            System.out.printf("Peso ideal Masculino: %.2f", pesoIdealMasculino);
+        } else {
+            double pesoIdealFeminino = (altura * 62.1) - 44.7;
+            System.out.printf("Peso ideal Feminino: %.2f", pesoIdealFeminino);
+        }
+    }
+}
